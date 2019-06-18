132807e84ea0559971d13f91c7e56319b3f0a074
anandb7248 <anandb7248@gmail.com>
2019-06-17 22:37:17 -0700

    complete post request to Mailchimp API

--- 8_APIs/Newsletter-Signup/app.js
+++ 8_APIs/Newsletter-Signup/app.js
@@ -6,21 +6,63 @@ const request = require('request');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   var firstName = req.body.fName;
   var lastName = req.body.lName;
   var email = req.body.email;
 
+  var data = {
+    'members': [
+      {
+      email_address: email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+        }
+      }
+    ],
+  }
+
+  var jsonData = JSON.stringify(data)
+
   console.log(firstName, lastName, email);
+
+  // NOTE: The API KEY BELOW HAS BEEN DISABLED ON MAILCHIMP
+  //       AS THIS CODE WILL BE PUSHED TO PUBLIC GITHUB
+
+  var options = {
+    url: 'https://us3.api.mailchimp.com/3.0/lists/c88fb2ef24',
+    method: 'POST',
+    headers: {
+      'Authorization': "anand1 8a8caf31357e4496e62d3e3690b8797b-us3"
+    },
+    body: jsonData
+  }
+
+  request(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+  });
 });
 
 app.listen(3000, function() {
   console.log("Server running on port 3000.");
 });
+
+// 8a8caf31357e4496e62d3e3690b8797b-us3
+
+// ID for List/Audience:
+// c88fb2ef24
