b776c73ef9c602a43b85ff9d9e952cf9a21f1098
anandb7248 <anandb7248@gmail.com>
2019-07-02 22:32:31 -0700

    complete challenge 5

--- 9_EJS/blog-website/app.js
+++ 9_EJS/blog-website/app.js
@@ -23,6 +23,17 @@ app.get("/", function(req, res) {
   });
 });
 
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
 
 
 
