dd68a329075a419d9db23273fee30754c26d2154
anandb7248 <anandb7248@gmail.com>
2019-07-02 22:03:21 -0700

    install npm dependecies. complete challenge 1

--- 9_EJS/blog-website/app.js
+++ 9_EJS/blog-website/app.js
@@ -15,10 +15,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
-
-
+app.get("/", function(req, res){
+  res.render("home");
+});
 
 
 
