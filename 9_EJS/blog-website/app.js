17e555ea0827c20b233bffdc016eff062624fa36
anandb7248 <anandb7248@gmail.com>
2019-07-02 22:14:47 -0700

    complete challenge 2

--- 9_EJS/blog-website/app.js
+++ 9_EJS/blog-website/app.js
@@ -12,11 +12,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home");
+app.get("/", function(req, res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
 });
 
 
