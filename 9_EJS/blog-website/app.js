176b8a552bf0a7274d5dffa050a8ac479276048c
anandb7248 <anandb7248@gmail.com>
2019-07-09 21:56:27 -0700

    complete challenge 11

--- 9_EJS/blog-website/app.js
+++ 9_EJS/blog-website/app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -21,6 +22,7 @@ app.get("/", function(req, res) {
   res.render("home", {
     homeStartingContent: homeStartingContent
   });
+  console.log(posts);
 });
 
 app.get("/about", function(req, res) {
@@ -48,6 +50,10 @@ app.post("/compose", function(req, res) {
     title: req.body.postTitle,
     body: req.body.postBody
   };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 
