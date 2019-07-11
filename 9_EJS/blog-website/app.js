83ac09bcd6d793248070645208c7a541e52ae256
anandb7248 <anandb7248@gmail.com>
2019-07-10 22:09:15 -0700

    complete challenge 18

--- 9_EJS/blog-website/app.js
+++ 9_EJS/blog-website/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const kebabCase = require('lodash/kebabCase');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -42,11 +43,13 @@ app.get("/compose", function(req, res) {
 });
 
 app.get("/posts/:postTitle", function(req, res) {
-  const requestedTitle = req.params.postTitle;
+  const requestedTitle = kebabCase(req.params.postTitle);
 
   posts.forEach(function(post) {
-    if (post.title === requestedTitle) {
+    const postTitle = kebabCase(post.title);
+    if (postTitle === requestedTitle) {
       console.log("Match Found!");
+      console.log(postTitle);
     }
   });
 });
