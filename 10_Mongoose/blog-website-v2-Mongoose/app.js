ccd04578fff896d794e84bcd3b96b1ed98b05b59
anandb7248 <anandb7248@gmail.com>
2019-08-20 19:13:58 -0700

    complete blog upgrade to use mongoose challenge

--- 10_Mongoose/blog-website-v2-Mongoose/app.js
+++ 10_Mongoose/blog-website-v2-Mongoose/app.js
@@ -4,13 +4,22 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const kebabCase = require('lodash/kebabCase');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser:true} );
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = [];
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 app.set('view engine', 'ejs');
 
@@ -20,9 +29,16 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+
+  Post.find(function(err, posts){
+    if(err){
+      console.log(err);
+    }else{
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
 });
 
@@ -42,12 +58,13 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:postTitle", function(req, res) {
-  const requestedTitle = kebabCase(req.params.postTitle);
+app.get("/posts/:postId", function(req, res) {
+  const postId = req.params.postId;
 
-  posts.forEach(function(post) {
-    const postTitle = kebabCase(post.title);
-    if (postTitle === requestedTitle) {
+  Post.findById(postId, function(err, post){
+    if(err){
+      console.log(err);
+    }else{
       res.render("post", {
         postTitle : post.title,
         postBody : post.body
@@ -61,18 +78,17 @@ app.post("/", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     body: req.body.postBody
-  };
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
+  post.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
-
-
-
 
 
 app.listen(3000, function() {
