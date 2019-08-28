c88accedd7b8261b7403254b9d369890453b67d8
anandb7248 <anandb7248@gmail.com>
2019-08-27 22:32:54 -0700

    DELETE All Articles

--- 8_APIs/Wiki-API/app.js
+++ 8_APIs/Wiki-API/app.js
@@ -54,6 +54,16 @@ app.post("/articles", function(req, res) {
   });
 });
 
+// DELETE All Articles
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    }else{
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
