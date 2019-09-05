40a2a0acb4464d8276ddcabc2ae75d297365b37f
anandb7248 <anandb7248@gmail.com>
2019-09-04 20:56:26 -0700

    DELETE a Specific Article

--- 8_APIs/Wiki-API/app.js
+++ 8_APIs/Wiki-API/app.js
@@ -139,7 +139,17 @@ app.route("/articles/:articleTitle")
       }
     }
   );
-});
+})
+
+  .delete(function(req, res){
+    Article.deleteOne({title: req.params.articleTitle}, function(err){
+      if(!err){
+        res.send("Successfully deleted article.");
+      }else{
+        res.send(err);
+      }
+    })
+  });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
