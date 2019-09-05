b333cb637cf8c6e23595338a4a064d5ff0e4f0a7
anandb7248 <anandb7248@gmail.com>
2019-09-04 20:52:48 -0700

    PATCH a Specific Article

--- 8_APIs/Wiki-API/app.js
+++ 8_APIs/Wiki-API/app.js
@@ -125,6 +125,20 @@ app.route("/articles/:articleTitle")
           res.send("Successfully updated article.")
         }
       });
+})
+
+.patch(function(req, res){
+  Article.update(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err){
+      if(!err){
+        res.send("Successfully updated article.");
+      }else{
+        res.send(err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
