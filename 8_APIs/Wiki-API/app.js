d1e6527b13e483df1b11fbe89a20026491358959
anandb7248 <anandb7248@gmail.com>
2019-09-04 20:46:23 -0700

    PUT a specific article

--- 8_APIs/Wiki-API/app.js
+++ 8_APIs/Wiki-API/app.js
@@ -103,7 +103,9 @@ app.route("/articles").get(function(req, res) {
   });
 });
 
-app.route("/articles/:articleTitle").get(function(req, res){
+app.route("/articles/:articleTitle")
+
+  .get(function(req, res){
   Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
     if(!err){
       res.send(foundArticle);
@@ -111,6 +113,18 @@ app.route("/articles/:articleTitle").get(function(req, res){
       res.send("No articles matching that title was found");
     }
   })
+})
+
+  .put(function(req, res){
+    Article.update(
+      {title: req.params.articleTitle},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      function(err){
+        if(!err){
+          res.send("Successfully updated article.")
+        }
+      });
 });
 
 app.listen(3000, function() {
