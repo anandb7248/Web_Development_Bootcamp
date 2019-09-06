025b4c9e3b91f5f477833742f9d696d5b29aa434
anandb7248 <anandb7248@gmail.com>
2019-09-06 12:41:08 -0700

    complete security level 1, resgiter users with username and password

--- 11_Authentication/Secrets/app.js
+++ 11_Authentication/Secrets/app.js
@@ -1,1 +1,76 @@
 //jshint esversion:6
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/credentials", {
+  useNewUrlParser: true
+});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render('home');
+});
+
+app.route("/login")
+  .get(function(req, res) {
+    res.render('login');
+  })
+  .post(function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser) {
+      if (err) {
+        res.send(err);
+      } else {
+        if(foundUser){
+          if(foundUser.password === password){
+            res.render('secrets');
+          }else{
+            res.send("Incorrect password");
+          }
+        }else{
+          res.send("Unable to find a user with that username/email");
+        }
+      }
+    });
+  });
+
+app.route("/register")
+  .get(function(req, res) {
+    res.render('register');
+  })
+  .post(function(req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save(function(err) {
+      if (!err) {
+        res.render('secrets');
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+})
