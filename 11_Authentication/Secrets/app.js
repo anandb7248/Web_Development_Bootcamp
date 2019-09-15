f68cc62f070a696673870d1d6cdb581e18738944
anandb7248 <anandb7248@gmail.com>
2019-09-14 20:56:22 -0700

    add environment variables

--- 11_Authentication/Secrets/app.js
+++ 11_Authentication/Secrets/app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,8 +23,8 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "not-so-secret-secret";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ['password']});
+
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
